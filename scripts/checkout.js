f77aafff47d8527b0bce0ddb25a154716c76fd45
hatrnuhn <yusron.ibnunaim@gmail.com>
2024-01-02 11:36:59 +0700

    Delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach(cartItem => {
 
   cartSummaryHTML += 
   `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -103,5 +103,11 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
       const productId = link.dataset.productId; // so somehow data-product-id is identified as dataset.productId, they automaticly convert it.
       removeFromCart(productId);
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      
+      container.remove();
     });
   });
